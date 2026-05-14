2b0b9789f1c72f852a4393c49e1964eaa00279fd
oguzhanaydiin <oguuzaydinn@gmail.com>
2026-05-14 16:10:34 +0200

    adding some changes and enhancements for main page (#9)

--- app/components/CuteIcon.vue
+++ app/components/CuteIcon.vue
@@ -3,47 +3,25 @@ const props = defineProps<{
   name: string
 }>()
 
-const failed = ref(false)
-const currentPathIndex = ref(0)
-
-const normalizedName = computed(() => {
-  return props.name
-    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
-    .toLowerCase()
-})
-
-const candidatePaths = computed(() => {
-  return [
-    `/icons/ui/${normalizedName.value}.svg`,
-    `/icons/status/${normalizedName.value}.svg`,
-    `/icons/${normalizedName.value}.svg`,
-  ]
-})
-
-const src = computed(() => candidatePaths.value[currentPathIndex.value])
-
-watch(() => props.name, () => {
-  currentPathIndex.value = 0
-  failed.value = false
-})
-
-function onImageError() {
-  if (currentPathIndex.value < candidatePaths.value.length - 1) {
-    currentPathIndex.value += 1
-  } else {
-    failed.value = true
-  }
+const ICON_PATHS: Record<string, string> = {
+  gym: '/icons/status/gym.svg',
+  cardio: '/icons/status/cardio.svg',
+  rest: '/icons/status/rest.svg',
 }
+
+const normalizedName = computed(() =>
+  props.name.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
+)
+
+const src = computed(() => ICON_PATHS[normalizedName.value] ?? `/icons/${normalizedName.value}.svg`)
 </script>
 
 <template>
   <img
-    v-if="!failed"
     :src="src"
     :alt="props.name"
     loading="lazy"
     decoding="async"
     draggable="false"
-    @error="onImageError"
   >
 </template>
